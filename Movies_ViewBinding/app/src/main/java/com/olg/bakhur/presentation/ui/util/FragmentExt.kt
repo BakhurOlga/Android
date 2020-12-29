d03217ca658d9293a1b31708e845fed157bb668f
Olga Bakhur <o.bakhur@vironit.co.uk>
2020-12-29 14:31:12 +0300

    3

--- Movies_ViewBinding/app/src/main/java/com/olg/bakhur/presentation/ui/util/FragmentExt.kt
+++ Movies_ViewBinding/app/src/main/java/com/olg/bakhur/presentation/ui/util/FragmentExt.kt
@@ -7,9 +7,8 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
-inline fun <reified T : ViewModel> Fragment.viewModel(
-    crossinline provider: () -> T
-) = viewModels<T> {
+inline fun <reified T : ViewModel> Fragment.viewModel(crossinline provider: () -> T) = viewModels<T> {
+
     object : ViewModelProvider.Factory {
         override fun <T : ViewModel?> create(modelClass: Class<T>): T = provider() as T
     }
